115d6341e0062cdbd08b1e4e4e40db66c10d256e
raymelguerra <108881451+raymelguerra@users.noreply.github.com>
2023-04-28 22:46:15 -0400

    test of cors

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ app = FastAPI()
 # Cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['http://localhost:4200'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
